f3d6e79f5cec0b264f675e7f33f0f115630deeee
Violeta Belcheva <v.a.belcheva@gmail.com>
2019-04-14 23:53:23 +0300

    add ability to get top words for specific hotel add __main__ to review_scoring module

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ reviews = csv_reader.getReviewsFromCsvFile("DB/Hotel_Reviews.csv")
 # print(len(reviews))
 
 topSize = 10
-positive, nagative = getTopPositiveNegativeTupple(topSize, reviews, 3)
+
+positive, nagative = getTopPositiveNegativeTupple(topSize, reviews, 3, None)
 print("Top " + str(topSize) + " Positive reviews words: ")
 pp.pprint(positive)
 print("Top " + str(topSize) + " Negative reviews words: ")
+pp.pprint(nagative)
+
+print("Top words for specific hotel:")
+hotelName = "Hotel Arena"
+positive, nagative = getTopPositiveNegativeTupple(topSize, reviews, 3, hotelName)
+print("Top " + str(topSize) + " Positive reviews words for " + hotelName +" : ")
+pp.pprint(positive)
+print("Top " + str(topSize) + " Negative reviews words for " + hotelName +" : ")
 pp.pprint(nagative)
--- review_scoring.py
+++ review_scoring.py
@@ -1,38 +1,42 @@
 import re
 import operator
+from csv_reader import getReviewsFromCsvFile
 
 positiveReveiwTag = "Positive_Review"
 negativeReveiwTag = "Negative_Review"
 reviewerScoreTag = "Reviewer_Score"
-positiveReviewThreshHold = "7"
+positiveReviewThreshHold = "5"
+hotelNameTag = "Hotel_Name"
 
 
-def getSortedWordOccurenceDict(tag, reviews):
+def getSortedWordOccurenceDict(tag, reviews, hotelName):
     positiveOccurencesDict = {}
     negativeOccurencesDict = {}
     for dbObject in reviews:
-        if (dbObject[reviewerScoreTag] > positiveReviewThreshHold):
-            reviewText = dbObject[tag].split(" ")
-            wordsInPositiveReview = list(filter(None, reviewText))
+        if (hotelName == None or dbObject[hotelNameTag] == hotelName):
+            if (dbObject[reviewerScoreTag] > positiveReviewThreshHold):
+                reviewText = dbObject[tag].split(" ")
+                wordsInPositiveReview = list(filter(None, reviewText))
 
-            for word in wordsInPositiveReview:
-                key = word.lower()
-                oldValue = positiveOccurencesDict.get(key)
-                occurences = 0 if oldValue == None else oldValue
-                positiveOccurencesDict.update(
-                    {key: occurences + reviewText.count(word)})
-        else:
-            reviewText = dbObject[tag].split(" ")
-            wordsInNegativeReview = list(filter(None, reviewText))
+                for word in wordsInPositiveReview:
+                    key = word.lower()
+                    oldValue = positiveOccurencesDict.get(key)
+                    occurences = 0 if oldValue == None else oldValue
+                    positiveOccurencesDict.update(
+                        {key: occurences + reviewText.count(word)})
+            else:
+                reviewText = dbObject[tag].split(" ")
+                wordsInNegativeReview = list(filter(None, reviewText))
 
-            for word in wordsInNegativeReview:
-                key = word.lower()
-                oldValue = negativeOccurencesDict.get(key)
-                occurences = 0 if oldValue == None else oldValue
-                negativeOccurencesDict.update(
-                    {key: occurences + reviewText.count(word)})
+                for word in wordsInNegativeReview:
+                    key = word.lower()
+                    oldValue = negativeOccurencesDict.get(key)
+                    occurences = 0 if oldValue == None else oldValue
+                    negativeOccurencesDict.update(
+                        {key: occurences + reviewText.count(word)})
 
     return sorted(positiveOccurencesDict.items(), key=operator.itemgetter(1), reverse=True), sorted(negativeOccurencesDict.items(), key=operator.itemgetter(1), reverse=True)
+
 
 def makeIsLongerThan(lengthOfWord):
     def isLongerThan(x):
@@ -42,12 +46,20 @@ def makeIsLongerThan(lengthOfWord):
             return True
     return isLongerThan
 
-def getTopPositiveNegativeTupple(size, reviews, resultWordLen):
-    filterFunc = makeIsLongerThan(3 if resultWordLen == None else resultWordLen)
+
+def getTopPositiveNegativeTupple(size, reviews, resultWordLen, hotelName):
+    filterFunc = makeIsLongerThan(
+        3 if resultWordLen == None else resultWordLen)
     positiveReviewWordList, negativeReviewWordList = getSortedWordOccurenceDict(
-        positiveReveiwTag, reviews)
+        positiveReveiwTag, reviews, hotelName)
 
     if (size != None):
         return list(filter(filterFunc, list(d[0] for d in positiveReviewWordList)))[:size],  list(filter(filterFunc, list(d[0] for d in negativeReviewWordList)))[:size]
     else:
         return list(filter(filterFunc, list(d[0] for d in positiveReviewWordList))),  list(filter(filterFunc, list(d[0] for d in negativeReviewWordList)))
+
+
+if __name__ == "__main__":
+    reviews = getReviewsFromCsvFile("DB/Hotel_Reviews.csv")
+    positive, nagative = getTopPositiveNegativeTupple(
+        10, reviews, 5, "Hotel Arena")
