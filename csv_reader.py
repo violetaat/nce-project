0bac7c5969ec37279199fdb60bc8fae0e81183ca
Violeta Belcheva <v.a.belcheva@gmail.com>
2019-04-14 23:33:41 +0300

    Removed debugging code that limits data set to 100

--- csv_reader.py
+++ csv_reader.py
@@ -7,13 +7,13 @@ def getReviewsFromCsvFile(path):
         with open(path, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
 
-            numberStop = 100
-            i = 0
+            # numberStop = 100
+            # i = 0
             for row in reader:
                 # TODO: Remove for production
-                i+=1
-                if(i > numberStop):
-                    break
+                # i+=1
+                # if(i > numberStop):
+                #     break
                 # ------
 
                 reviews.append(dict(row))
